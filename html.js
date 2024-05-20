81e11b268e7fa098ab6e78e0153b1457e32f45a7
DenisseDiaz <96696742+DenisseDiaz@users.noreply.github.com>
2024-05-20 16:21:42 -0600

    Add files via upload

---
+++ html.js
@@ -0,0 +1,38 @@
+// Reseña de mis Pasatiempos
+//Te mostrare mis pasatiempos favoritos a partir de un codigo HTML
+
+//Variables
+
+<DocumentType>
+
+<html>
+ <head>
+ <link rel="foto" type="text/css" href="style.css"></link>
+ </head>
+ <body >
+
+    <tittle>MI PRIMER PASATIEMPO</tittle>
+    <h1>MI PASION POR LA LECTURA</h1>
+    background-image: url(leer.jpg);
+
+  <p>Una de mis grandes pasiones es la lectura y la escritura. Me encanta sumergirme en mundos ficticios y dejar volar mi imaginación. Desde novelas de ciencia ficción hasta poesía contemporánea, disfruto de una amplia gama de géneros. Además, escribir me permite expresar mis pensamientos y emociones, creando historias y personajes que cobran vida en cada palabra.
+</p>
+
+    <tittle>MI SEGUNDO PASATIEMPO</tittle>
+    <h2>MI PASION POR EXPLORAR LA NATURALEZA</h2>
+    background-image: url(naturaleza.jpg);
+
+    <p>Salir al aire libre y explorar la naturaleza es otra de mis actividades favoritas. Ya sea hacer senderismo en las montañas, pasear por la playa o simplemente disfrutar de un picnic en el parque, siempre encuentro paz y tranquilidad en el entorno natural. La belleza del paisaje y el aire fresco revitalizan mi espíritu y me inspiran profundamente.</p>
+
+    <tittle>MI TERCER PASATIEMPO</tittle>
+    <h2>MI PASION POR LA FOTOGRAFIA</h2>
+    background-image: url(Foto_2023-03-03_164710.jpg);
+
+    <p>La fotografía es una manera maravillosa de capturar momentos especiales y la belleza del mundo que nos rodea. Me encanta experimentar con diferentes ángulos y luces para crear imágenes únicas y significativas. Mis temas favoritos son la naturaleza y la vida urbana, siempre buscando nuevas perspectivas y detalles que a menudo pasan desapercibidos.</p>
+    <p>Para más información sobre mis hobbies, visita mi <a href="https://miwebpersonal.com">sitio web personal</a> o mi <a href="https://miportafoliofotografia.com">portafolio de fotografía</a>.</p>
+
+ </body>
+
+</html>
+
+</DocumentType>
